324601ac10ac349805c89f9d8557bf1ce6ce18c9
Vasya <vasya.454@gmail.com>
2023-04-17 15:31:58 +0300

    Time limit exceeded

--- src/Main.java
+++ src/Main.java
@@ -6,43 +6,39 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(new File("INPUT.txt"));
-            int n = scanner.nextInt();
-            ArrayList<Gnome> gnomes = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                gnomes.add(i, new Gnome(i + 1));
-                gnomes.get(i).a = scanner.nextInt();
-            }
-            for (int i = 0; i < n; i++) {
-                gnomes.get(i).b = scanner.nextInt();
-            }
-            scanner.close();
-            gnomes.sort(new Comparator<Gnome>() {
-                @Override
-                public int compare(Gnome o1, Gnome o2) {
-                    return Integer.compare((o2.b - o1.a), (o1.b - o2.a));
-                }
-            });
-            String out_str = "";
-            int comeToBedTotalTime = 0;
-            for (Gnome g :
-                    gnomes) {
-                comeToBedTotalTime += g.a;
-                if (comeToBedTotalTime - gnomes.get(0).a < gnomes.get(0).b) {
-                    out_str += g.index + " ";
-                } else {
-                    out_str = "-1";
-                    break;
-                }
-            }
-            FileWriter fileWriter = new FileWriter("OUTPUT.txt");
-            fileWriter.write(out_str);
-            fileWriter.close();
-        } catch (IOException e) {
-            System.out.println("Something wrong");
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(new File("INPUT.txt"));
+        int n = scanner.nextInt();
+        ArrayList<Gnome> gnomes = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            gnomes.add(i, new Gnome(i + 1));
+            gnomes.get(i).a = scanner.nextInt();
         }
+        for (int i = 0; i < n; i++) {
+            gnomes.get(i).b = scanner.nextInt();
+        }
+        scanner.close();
+        gnomes.sort(new Comparator<Gnome>() {
+            @Override
+            public int compare(Gnome o1, Gnome o2) {
+                return Integer.compare((o2.b - o1.a), (o1.b - o2.a));
+            }
+        });
+        String out_str = "";
+        int comeToBedTotalTime = 0;
+        for (Gnome g :
+                gnomes) {
+            comeToBedTotalTime += g.a;
+            if (comeToBedTotalTime - gnomes.get(0).a < gnomes.get(0).b) {
+                out_str += g.index + " ";
+            } else {
+                out_str = "-1";
+                break;
+            }
+        }
+        FileWriter fileWriter = new FileWriter("OUTPUT.txt");
+        fileWriter.write(out_str);
+        fileWriter.close();
     }
 }
 
